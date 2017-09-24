b0a56d15fdeeb0f21359658e0142f2302225f282
Danielle Colbert <dbcolber@umich.edu>
2017-09-24 14:40:46 -0400

    Problem 1 Done

--- SI364-HW2.py
+++ SI364-HW2.py
@@ -7,16 +7,52 @@
 
 ## [PROBLEM 1]
 
-## Edit the following Flask application code so that if you run the application locally and got to the URL http://localhost:5000/question, you see a form that asks you to enter your favorite number. Once you enter a number and submit it to the form, you should then see a web page that says "Double your favorite number is <number>". For example, if you enter 2 into the form, you should then see a page that says "Double your favorite number is 4". Careful about types in your Python code!
+## Edit the following Flask application code so that if you run the application locally and go 
+# to the URL http://localhost:5000/question, you see a form that asks you to enter your favorite number. 
+# Once you enter a number and submit it to the form, you should then see a web page that says "Double your 
+# favorite number is <number>". For example, if you enter 2 into the form, you should then see a page that says 
+# "Double your favorite number is 4". Careful about types in your Python code!
+
 ## You can assume a user will always enter a number only.
 
-from flask import Flask
+import requests
+
+from flask import Flask, request
 app = Flask(__name__)
 app.debug = True
 
 @app.route('/')
 def hello_to_you():
     return 'Hello!'
+
+@app.route('/question')
+def ask_question():
+	s = """<!DOCTYPE html>
+<html>
+<body>
+
+<form action="http://localhost:5000/result" method="GET">
+  Please enter your favorite number:<br>
+  <input type="text" name="number" value="">
+  <br><br>
+  <input type="submit" value="Submit">
+</form> 
+
+</body>
+</html>""" 
+	return s
+
+@app.route('/result',methods = ['POST', 'GET'])
+def res():
+    if request.method == 'GET':
+        result = request.args
+        number1 = result.get('number')
+        double = 2 * (int(number1))
+        return "<b>" + "Double your favorite {0} is {1}".format(number1, double)
+
+## MY INTERACTIVE DATA EXCHANGE
+
+
 
 
 if __name__ == '__main__':
@@ -25,7 +61,10 @@ if __name__ == '__main__':
 
 ## [PROBLEM 2]
 
-## Come up with your own interactive data exchange that you want to see happen dynamically in the Flask application, and build it into the above code for a Flask application. It should:
+## Come up with your own interactive data exchange that you want to see happen dynamically in the Flask application, 
+## and build it into the above code for a Flask application. 
+
+# It should:
 # - not be an exact repeat of something you did in class, but it can be similar
 # - should include an HTML form (of any kind: text entry, radio button, checkbox... feel free to try out whatever you want)
 # - should, on submission of data to the HTML form, show new data that depends upon the data entered into the submission form (text entered, radio button selected, etc). So if a user has to enter a number, it should do an operation on that number. If a user has to select a radio button representing a song name, it should do a search for that song in an API.
